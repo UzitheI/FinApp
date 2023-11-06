2c52021c116885fda35e4fe38e8a35316be2cd56
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-11-06 16:21:56 +0545

    first version

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+
+function Main(){
+  return(
+
+    <div>
+    <App/>
+  </div>
+    )
+}
+ReactDOM.render(<Main/>,document.getElementById("root"))
